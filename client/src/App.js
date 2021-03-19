09e702c30b4f18dc24ff2c67477cd8fde81571d0
shahin1290 <s.patowary@yahoo.com>
2021-03-19 22:53:13 +0100

    initial commit setsup with react app, styled-components and adds navbar component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import {
+  Nav,
+  NavLogo,
+  NavbarContainer,
+  NavItem,
+  NavMenu,
+  NavLinks,
+  MobileIcon
+} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>dollar</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>HOME</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='about'>ABOUT US</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to='about'>SERVICES</NavLinks>
+            </NavItem>
+
+
+            <NavItem>
+              <NavLinks to='about'>PAGES</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to='about'>BLOG</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='about'>CONTACT</NavLinks>
+            </NavItem>
+
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
